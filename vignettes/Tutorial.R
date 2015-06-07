f58805d570224a87fe8567742c3fc4e6b5ada2b6
dmpe <cincenko@seznam.cz>
2015-06-07 13:27:52 +0200

    update

--- vignettes/Tutorial.R
+++ vignettes/Tutorial.R
@@ -1,7 +1,19 @@
-## ---- fig.show='hold'----------------------------------------------------
-plot(1:10)
-plot(10:1)
+## ------------------------------------------------------------------------
+rbitlyApi("0906523ec6a8c78b33f9310e84e7a5c81e500909")
 
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(head(mtcars, 10))
+user.metrics.popular_links(unit = "month", units = -1, limit = 100)
+
+## ------------------------------------------------------------------------
+rbitlyApi("0906523ec6a8c78b33f9310e84e7a5c81e500909")
+link.metrics.encoders_by_count(link = "http://bit.ly/DPetrov", my_network = "false", limit = 100)
+
+## ------------------------------------------------------------------------
+rbitlyApi("0906523ec6a8c78b33f9310e84e7a5c81e500909")
+user.info()
+
+user.tracking_domain_list()
+
+## ------------------------------------------------------------------------
+rbitlyApi("0906523ec6a8c78b33f9310e84e7a5c81e500909")
+bitly_pro_domain(domain = "nyti.ms")
 
