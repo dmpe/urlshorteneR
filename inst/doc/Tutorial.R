43193d8f6c10816fd0b0e83e5046a720e0662a63
dmpe <cincenko@seznam.cz>
2015-06-07 15:12:19 +0200

    vignette

---
+++ inst/doc/Tutorial.R
@@ -0,0 +1,20 @@
+## ------------------------------------------------------------------------
+library(RBitly)
+rbitlyApi("0906523ec6a8c78b33f9310e84e7a5c81e500909")
+
+user.metrics.popular_links(unit = "month", units = -1, limit = 100)
+
+## ------------------------------------------------------------------------
+rbitlyApi("0906523ec6a8c78b33f9310e84e7a5c81e500909")
+link.metrics.encoders_by_count(link = "http://bit.ly/DPetrov", my_network = "false", limit = 100)
+
+## ------------------------------------------------------------------------
+rbitlyApi("0906523ec6a8c78b33f9310e84e7a5c81e500909")
+user.info()
+
+user.tracking_domain_list()
+
+## ------------------------------------------------------------------------
+rbitlyApi("0906523ec6a8c78b33f9310e84e7a5c81e500909")
+bitly_pro_domain(domain = "nyti.ms")
+
