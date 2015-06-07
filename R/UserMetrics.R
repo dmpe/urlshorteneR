722bd9757c42d280c30914e91f8c10f306ad03fa
dmpe <cincenko@seznam.cz>
2015-06-07 16:49:31 +0200

    rename functions 2/x [skip ci]

--- R/UserMetrics.R
+++ R/UserMetrics.R
@@ -41,7 +41,7 @@ user_Metrics_Clicks <- function(limit = 1000, unit = c("minute", "hour", "day", 
   
   if (rollup == "true") {
     
-    # no data frame
+    # won't return a data frame, just a number
     return(df_user_metrics_clicks_data)
     
   } else {
@@ -71,36 +71,36 @@ user_Metrics_Clicks <- function(limit = 1000, unit = c("minute", "hour", "day", 
 #' 
 #' @examples
 #' rbitlyApi("0906523ec6a8c78b33f9310e84e7a5c81e500909")
-#' user.metrics.countries(unit = "day", units = -1, limit = 100, rollup = "true")
-#' user.metrics.countries(unit = "day", units = -1, limit = 100, rollup = "false")
+#' user_Metrics_Countries(unit = "day", units = -1, limit = 100, rollup = "true")
+#' user_Metrics_Countries(unit = "day", units = -1, limit = 100, rollup = "false")
 #' 
 #' @export
 user_Metrics_Countries <- function(limit = 1000, unit = c("minute", "hour", "day", "week", "month"), 
                                    rollup = c("false", "true"), units = -1) {
-  unit.matched <- match.arg(unit)
-  rollup.matched <- match.arg(rollup)
+  unit_matched <- match.arg(unit)
+  rollup_matched <- match.arg(rollup)
   
-  user.metrics.countries.url <- "https://api-ssl.bitly.com/v3/user/countries"
+  user_metrics_countries_url <- "https://api-ssl.bitly.com/v3/user/countries"
   
-  created_URL <- paste(user.metrics.countries.url, "?limit=", limit, "&unit=", unit.matched, 
-                       "&units=", units, "&rollup=", rollup.matched, sep = "")
+  created_URL <- paste(user_metrics_countries_url, "?limit=", limit, "&unit=", unit_matched, 
+                       "&units=", units, "&rollup=", rollup_matched, sep = "")
   created_URL <- paste(created_URL, "&format=json", sep = "")
   
   # call method from ApiKey.R
-  df.user.metrics.countries <- doRequest(created_URL)
+  df_user_metrics_countries <- doRequest(created_URL)
   
-  df.user.metrics.countries.data <- df.user.metrics.countries$data$user_countries
+  df_user_metrics_countries_data <- df_user_metrics_countries$data$user_countries
   
   # More testing required
   if (rollup == "true") {
-    return(df.user.metrics.countries.data)
+    return(df_user_metrics_countries_data)
     
   } else {
     # https://stackoverflow.com/questions/4227223/r-list-to-data-frame
-    df.user.metrics.countries.data <- data.frame(sapply(df.user.metrics.countries.data,c))
+    df_user_metrics_countries_data <- data.frame(sapply(df_user_metrics_countries_data,c))
   }
   
-  return(df.user.metrics.countries.data)
+  return(df_user_metrics_countries_data)
 }
 
 #' @title Returns the authenticated user's most-clicked Bitlinks (ordered by number of clicks) in a given time period.
@@ -119,25 +119,24 @@ user_Metrics_Countries <- function(limit = 1000, unit = c("minute", "hour", "day
 #' 
 #' @examples
 #' rbitlyApi("0906523ec6a8c78b33f9310e84e7a5c81e500909")
-#' user.metrics.popular_links(unit = "day", units = -1, limit = 100)
+#' user_Metrics_PopularLinks(unit = "day", units = -1, limit = 100)
 #' 
 #' @export
 user_Metrics_PopularLinks <- function(limit = 1000, unit = c("minute", "hour", "day", "week", "month"),
                                        units = -1) {
-  unit.matched <- match.arg(unit)
+  unit_matched <- match.arg(unit)
   
-  user.metrics.popular_links.url <- "https://api-ssl.bitly.com/v3/user/popular_links"
+  user_metrics_popular_links_url <- "https://api-ssl.bitly.com/v3/user/popular_links"
   
-  created_URL <- paste(user.metrics.popular_links.url, "?limit=", limit, "&unit=", unit.matched, 
+  created_URL <- paste(user_metrics_popular_links_url, "?limit=", limit, "&unit=", unit_matched, 
                        "&units=", units, sep = "")
   created_URL <- paste(created_URL, "&format=json", sep = "")
   
   # call method from ApiKey.R
-  user.metrics.popular_links <- doRequest(created_URL)
+  df_user_metrics_popular_links <- doRequest(created_URL)
+  df_user_metrics_popular_links_data <- df_user_metrics_popular_links$data$popular_links
   
-  user.metrics.popular_links.data <- user.metrics.popular_links$data$popular_links
-  
-  return(user.metrics.popular_links.data)
+  return(df_user_metrics_popular_links_data)
   
 }
 
@@ -161,37 +160,36 @@ user_Metrics_PopularLinks <- function(limit = 1000, unit = c("minute", "hour", "
 #' 
 #' @examples
 #' rbitlyApi("0906523ec6a8c78b33f9310e84e7a5c81e500909")
-#' user.metrics.referrers(unit = "day", units = -1, limit = 100, rollup = "true")
-#' user.metrics.referrers(unit = "day", units = -1, limit = 100, rollup = "false")
+#' user_Metrics_Referrers(unit = "day", units = -1, limit = 100, rollup = "true")
+#' user_Metrics_Referrers(unit = "day", units = -1, limit = 100, rollup = "false")
 #' 
 #' @export
 user_Metrics_Referrers <- function(limit = 1000, unit = c("minute", "hour", "day", "week", "month"), 
                                    rollup = c("false", "true"), units = -1) {
-  unit.matched <- match.arg(unit)
-  rollup.matched <- match.arg(rollup)
+  unit_matched <- match.arg(unit)
+  rollup_matched <- match.arg(rollup)
   
-  user.metrics.referrers.url <- "https://api-ssl.bitly.com/v3/user/referrers"
+  user_metrics_referrers_url <- "https://api-ssl.bitly.com/v3/user/referrers"
   
-  created_URL <- paste(user.metrics.referrers.url, "?limit=", limit, "&unit=", unit.matched, 
-                       "&units=", units, "&rollup=", rollup.matched, sep = "")
+  created_URL <- paste(user_metrics_referrers_url, "?limit=", limit, "&unit=", unit_matched, 
+                       "&units=", units, "&rollup=", rollup_matched, sep = "")
   created_URL <- paste(created_URL, "&format=json", sep = "")
   
   # call method from ApiKey.R
-  user.metrics.referrers.url <- doRequest(created_URL)
-  
-  user.metrics.referrers.data <- user.metrics.referrers.url$data$user_referrers
+  df_user_metrics_referrers <- doRequest(created_URL)
+  df_user_metrics_referrers_data <- df_user_metrics_referrers$data$user_referrers
   
   if (rollup == "true") {
-    user.metrics.referrers.data <- data.frame(t(sapply(user.metrics.referrers.data,c)))
+    df_user_metrics_referrers_data <- data.frame(t(sapply(df_user_metrics_referrers_data,c)))
     
   } else {
     # No way I can check at the moment 
     
     # https://stackoverflow.com/questions/4227223/r-list-to-data-frame
-    user.metrics.referrers.data <- data.frame(t(sapply(user.metrics.referrers.data,c)))
+    df_user_metrics_referrers_data <- data.frame(t(sapply(df_user_metrics_referrers_data,c)))
   }
   
-  return(user.metrics.referrers.data)
+  return(df_user_metrics_referrers_data)
   
 }
 
@@ -221,40 +219,39 @@ user_Metrics_Referrers <- function(limit = 1000, unit = c("minute", "hour", "day
 #' 
 #' @examples
 #' rbitlyApi("0906523ec6a8c78b33f9310e84e7a5c81e500909")
-#' user.metrics.referring_domains(unit = "day", units = -1, limit = 100, rollup = "true")
-#' user.metrics.referring_domains(unit = "day", units = -1, limit = 100, rollup = "false")
-#' user.metrics.referring_domains(unit = "day", units = -1, limit = 100, 
+#' user_Metrics_ReferringDomains(unit = "day", units = -1, limit = 100, rollup = "true")
+#' user_Metrics_ReferringDomains(unit = "day", units = -1, limit = 100, rollup = "false")
+#' user_Metrics_ReferringDomains(unit = "day", units = -1, limit = 100, 
 #' exclude_social_networks = "false")
-#' user.metrics.referring_domains(unit = "day", units = -1, limit = 100, 
+#' user_Metrics_ReferringDomains(unit = "day", units = -1, limit = 100, 
 #' exclude_social_networks = "true")
 #' 
 #' @export
 user_Metrics_ReferringDomains <- function(limit = 1000, unit = c("minute", "hour", "day", "week", "month"),
-                                           rollup = c("false", "true"), units = -1, 
-                                           exclude_social_networks = c("true", "false"), login=NULL) {
+                                           rollup = c("false", "true"), units = -1, login = NULL, 
+                                           exclude_social_networks = c("true", "false")) {
   
-  unit.matched <- match.arg(unit)
-  rollup.matched <- match.arg(rollup)
+  unit_matched <- match.arg(unit)
+  rollup_matched <- match.arg(rollup)
   exclude_social_networks.matched <- match.arg(exclude_social_networks)
   
-  user.metrics.referring_domains.url <- "https://api-ssl.bitly.com/v3/user/referring_domains"
+  user_metrics_referring_domains_url <- "https://api-ssl.bitly.com/v3/user/referring_domains"
   
-  created_URL <- paste(user.metrics.referring_domains.url, "?limit=", limit, "&unit=", unit.matched, 
-                       "&units=", units, "&rollup=", rollup.matched, sep = "")
+  created_URL <- paste(user_metrics_referring_domains_url, "?limit=", limit, "&unit=", unit_matched, 
+                       "&units=", units, "&rollup=", rollup_matched, sep = "")
   created_URL <- paste(created_URL, "&exclude_social_networks=", exclude_social_networks.matched, 
                        "&login=", login, "&format=json", sep = "")
   
   # call method from ApiKey.R
-  user.metrics.referring_domains <- doRequest(created_URL)
+  df_user_metrics_referring_domains <- doRequest(created_URL)
+  df_user_metrics_referring_domains_data <- df_user_metrics_referring_domains$data$user_referring_domains
   
-  user.metrics.referring_domains.data <- user.metrics.referring_domains$data$user_referring_domains
-  
-  if (length(user.metrics.referring_domains.data) == 0) {
-    user.metrics.referring_domains.data <- NULL
+  if (length(df_user_metrics_referring_domains_data) == 0) {
+    df_user_metrics_referring_domains_data <- NULL
     message("You have zero referring domains given your function input.")
   }
   
-  return(user.metrics.referring_domains.data)
+  return(df_user_metrics_referring_domains_data)
   
 }
 
@@ -280,35 +277,35 @@ user_Metrics_ReferringDomains <- function(limit = 1000, unit = c("minute", "hour
 #' 
 #' @examples
 #' rbitlyApi("0906523ec6a8c78b33f9310e84e7a5c81e500909")
-#' user.metrics.shorten_counts(unit = "day", units = -1, limit = 100, rollup = "true")
-#' user.metrics.shorten_counts(unit = "day", units = -1, limit = 100, rollup = "false")
-#' user.metrics.shorten_counts(unit = "day", units = -1, limit = 100)
+#' user_Metrics_ShortenCounts(unit = "day", units = -1, limit = 100, rollup = "true")
+#' user_Metrics_ShortenCounts(unit = "day", units = -1, limit = 100, rollup = "false")
+#' user_Metrics_ShortenCounts(unit = "day", units = -1, limit = 100)
 #' 
 #' @export
 user_Metrics_ShortenCounts <- function(limit = 1000, unit = c("minute", "hour", "day", "week", "month"),
                                         rollup = c("false", "true"), units = -1) {
   
-  unit.matched <- match.arg(unit)
-  rollup.matched <- match.arg(rollup)
+  unit_matched <- match.arg(unit)
+  rollup_matched <- match.arg(rollup)
   
-  user.metrics.shorten_counts.url <- "https://api-ssl.bitly.com/v3/user/shorten_counts"
+  user_metrics_shorten_counts_url <- "https://api-ssl.bitly.com/v3/user/shorten_counts"
   
-  created_URL <- paste(user.metrics.shorten_counts.url, "?limit=", limit, "&unit=", unit.matched, 
-                       "&units=", units, "&rollup=", rollup.matched, sep = "")
+  created_URL <- paste(user_metrics_shorten_counts_url, "?limit=", limit, "&unit=", unit_matched, 
+                       "&units=", units, "&rollup=", rollup_matched, sep = "")
   created_URL <- paste(created_URL, "&format=json", sep = "")
   
   # call method from ApiKey.R
-  user.metrics.shorten_counts <- doRequest(created_URL)
+  df_user_metrics_shorten_counts <- doRequest(created_URL)
   
   if (rollup.matched == "false") {
-    user.metrics.shorten_counts.data <- user.metrics.shorten_counts$data$user_shorten_counts
-    user.metrics.shorten_counts.data$dt <- as.POSIXct(as.integer(user.metrics.shorten_counts.data$dt), 
+    df_user.metrics_shorten_counts_data <- df_user_metrics_shorten_counts$data$user_shorten_counts
+    df_user.metrics_shorten_counts_data$dt <- as.POSIXct(as.integer(df_user.metrics_shorten_counts_data$dt), 
                                                       origin = "1970-01-01", tz = "UTC")
     
   } else {
-    user.metrics.shorten_counts.data <- data.frame(t(sapply(user.metrics.shorten_counts$data,c)))
+    df_user.metrics_shorten_counts_data <- data.frame(t(sapply(df_user_metrics_shorten_counts$data,c)))
   }
   
-  return(user.metrics.shorten_counts.data)
+  return(df_user.metrics_shorten_counts_data)
 }
 
--- tests/testthat/test-UserMetrics.R
+++ tests/testthat/test-UserMetrics.R
@@ -9,58 +9,57 @@ rbitlyApi("0906523ec6a8c78b33f9310e84e7a5c81e500909")
 context("User Metrics")
 
 test_that("Returns aggregate metrics about the countries referring click traffic to all of the authenticated user's Bitlinks.", {
-  umcoun <- user.metrics.countries(unit = "day", units = -1, limit = 100, rollup = "true")
+  umcoun <- user_Metrics_Countries(unit = "day", units = -1, limit = 100, rollup = "true")
   expect_named(umcoun, c("country","clicks")) 
 })
 
 test_that("Returns aggregate metrics about the countries referring click traffic to all of the authenticated user's Bitlinks.", {
-  umcounc <- user.metrics.countries(unit = "day", units = -1, limit = 100, rollup = "false")
+  umcounc <- user_Metrics_Countries(unit = "day", units = -1, limit = 100, rollup = "false")
   expect_named(umcounc, c("country","clicks")) # "url" doens't need to be in there (semi-optional)
 })
 
 test_that("Returns the aggregate number of clicks on all of the authenticated user's Bitlinks.", {
-  umc <- user.metrics.clicks(unit = "day", units = -1, limit = 100, rollup = "true")
+  umc <- user_Metrics_Clicks(unit = "day", units = -1, limit = 100, rollup = "true")
 })
 
 test_that("Returns the aggregate number of clicks on all of the authenticated user's Bitlinks.", {
-  umcc <- user.metrics.clicks(unit = "day", units = -1, limit = 100, rollup = "false")
+  umcc <- user_Metrics_Clicks(unit = "day", units = -1, limit = 100, rollup = "false")
   expect_named(umcc, c("dt", "clicks")) # "url" doens't need to be in there (semi-optional)
 })
 
 test_that("Returns the authenticated user's most-clicked Bitlinks (ordered by number of clicks) in a given time period.", {
-  umpl <- user.metrics.popular_links(unit = "month", units = -1, limit = 100)
+  umpl <- user_Metrics_PopularLinks(unit = "month", units = -1, limit = 100)
   expect_named(umpl, c("link", "clicks")) 
 })
 
 
 test_that("Returns aggregate metrics about the pages referring click traffic to all of the authenticated user's Bitlinks.", {
-  umr <- user.metrics.referrers(unit = "day", units = -1, limit = 100, rollup = "false")
+  umr <- user_Metrics_Referrers(unit = "day", units = -1, limit = 100, rollup = "false")
   expect_named(umr, c("referrer", "clicks")) 
 })
 
 test_that("Returns aggregate metrics about the pages referring click traffic to all of the authenticated user's Bitlinks.", {
-  umrr <- user.metrics.referrers(unit = "day", units = -1, limit = 100, rollup = "true")
+  umrr <- user_Metrics_Referrers(unit = "day", units = -1, limit = 100, rollup = "true")
   expect_named(umrr, c("referrer", "clicks")) 
 })
 
 test_that("Returns aggregate metrics about the domains referring click traffic to all of the authenticated user's Bitlinks.", {
-  umrd <- user.metrics.referring_domains(unit = "day", units = -1, limit = 100, rollup = "true", exclude_social_networks = "false")
+  umrd <- user_Metrics_ReferringDomains(unit = "day", units = -1, limit = 100, rollup = "true", exclude_social_networks = "false")
   expect_named(umrd, c("domain", "clicks"))
 })
 
 test_that("Returns aggregate metrics about the domains referring click traffic to all of the authenticated user's Bitlinks.", {
-  umrdr <- user.metrics.referring_domains(unit = "day", units = -1, limit = 100, rollup = "true", exclude_social_networks = "true")
-  
+
   expect_message(user.metrics.referring_domains(unit = "day", units = -1, limit = 100, rollup = "true", exclude_social_networks = "true")
                  , "You have zero referring domains given your function input.") 
 })
 
 test_that("Returns the number of Bitlinks created in a given time period by the authenticated user.", {
-  umsc <- user.metrics.shorten_counts(unit = "day", units = -1, limit = 100, rollup = "true")
+  umsc <- user_Metrics_ShortenCounts(unit = "day", units = -1, limit = 100, rollup = "true")
   expect_equal(dim(umsc)[[2]], 5)
 })
 
 test_that("Returns the number of Bitlinks created in a given time period by the authenticated user.", {
-  umscf <- user.metrics.shorten_counts(unit = "day", units = -1, limit = 100, rollup = "false")
+  umscf <- user_Metrics_ShortenCounts(unit = "day", units = -1, limit = 100, rollup = "false")
   expect_named(umscf, c("dt", "shortens"))
 })
