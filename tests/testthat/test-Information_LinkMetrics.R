bb6639b2a030047d7d4f7d5b1b12dab304438650
dmpe <cincenko@seznam.cz>
2015-10-18 17:01:23 +0200

    update vig. too [skip ci]

--- tests/testthat/test-Information_LinkMetrics.R
+++ tests/testthat/test-Information_LinkMetrics.R
@@ -35,44 +35,44 @@ test_that("Returns the number of clicks on a single Bitlink.", {
 })
 
 test_that("Returns metrics about the countries referring click traffic to a single Bitlink.", {
-  lmcc <- link_Metrics_Countries(link = "http://bit.ly/DPetrov", unit = "day", units = -1, limit = 100)
+  lmcc <- bitly_LinksMetricsCountries(link = "http://bit.ly/DPetrov", unit = "day", units = -1, limit = 100)
   expect_named(lmcc, c("country", "clicks"))
 })
 
 test_that("Returns users who have encoded this long URL (optionally only those in the requesting user's social graph).", {
-  lme <- link_Metrics_Encoders(link = "http://bit.ly/DPetrov", my_network = "false", limit = 25)
+  lme <- bitly_LinksMetricsEncoders(link = "http://bit.ly/DPetrov", my_network = "false", limit = 25)
   expect_named(lme, c("link", "user", "ts")) 
 })
 
 test_that("Returns the number of users who have shortened (encoded) a single Bitlink.", {
-  lmec <- link_Metrics_EncodersCount(link = "http://bit.ly/DPetrov")
+  lmec <- bitly_LinksMetricsEncodersCount(link = "http://bit.ly/DPetrov")
   expect_named(lmec, c("count", "aggregate_link"))
 })
 
 test_that("Returns metrics about the domains referring click traffic to a single Bitlink.", {
-  lmebc <- link_Metrics_EncodersByCount(link = "http://bit.ly/DPetrov", my_network = "false", limit = 100)
+  lmebc <- bitly_LinksMetricsEncodersByCount(link = "http://bit.ly/DPetrov", my_network = "false", limit = 100)
   expect_named(lmebc, c("count", "link", "user", "ts")) 
 })
 
 test_that("Returns metrics about the domains referring click traffic to a single Bitlink.", {
-  lmrd <- link_Metrics_ReferringDomains(link = "http://bit.ly/DPetrov", unit = "day", units = -1, limit = 100)
+  lmrd <- bitly_LinksMetricsReferringDomains(link = "http://bit.ly/DPetrov", unit = "day", units = -1, limit = 100)
   expect_named(lmrd, c("domain", "clicks")) 
 })
 
 test_that("Returns metrics about the pages referring click traffic to a single Bitlink.", {
-  lmr <- link_Metrics_Referrers(link = "http://bit.ly/DPetrov", unit = "day", units = -1, limit = 100)
+  lmr <- bitly_LinksMetricsReferrers(link = "http://bit.ly/DPetrov", unit = "day", units = -1, limit = 100)
   expect_named(lmr, c( "referrer", "clicks"))
 })
 
 test_that("Returns metrics about the pages referring click traffic to a single Bitlink, grouped by referring domain.", {
-  lmrbd <- link_Metrics_ReferrersByDomain(link = "http://bit.ly/DPetrov", unit = "day", units = -1, limit = 100)
+  lmrbd <- bitly_LinksMetricsReferrersByDomain(link = "http://bit.ly/DPetrov", unit = "day", units = -1, limit = 100)
   expect_named(lmrbd, c( "referrer", "clicks", "type"))
 })
 
 context("Domains")
 
 test_that("Query whether a given domain is a valid bitly pro domain. ", {
-  expect_message(bitly_pro_domain(domain = "nytidsfds.ms"), "NOT")
-  expect_message(bitly_pro_domain(domain = "nyti.ms"), "is") 
+  expect_message(bitly_IsProDomain(domain = "nytidsfds.ms"), "NOT")
+  expect_message(bitly_IsProDomain(domain = "nyti.ms"), "is") 
 })
 
