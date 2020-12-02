9bb29ec4b66c77289a19aaaac69e2d40eb5e42c8
Stephen Synchronicity <sholsen@alumni.emory.edu>
2020-12-01 20:48:45 -0500

    negate is_token for condition to check for path

--- R/ApiKey.R
+++ R/ApiKey.R
@@ -67,7 +67,7 @@ is_token <- function(x) inherits(x, "Token")
 bitly_auth <- function(key = "be03aead58f23bc1aee6e1d7b7a1d99d62f0ede8",
                       secret = "f9c6a3b18968e991e35f466e90c7d883cc176073", debug = F, token) {
   if (!missing(token)) {
-    if (is_token(token)) {
+    if (!is_token(token)) {
       token <- readRDS(token)
       if (!is_token(token)) stop("Invalid token")
     }
